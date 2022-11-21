24fa532843914c5415d5d7bf431566de0a69e071
Jair Carbajal <jair.carbajal91@gmail.com>
2022-11-21 13:28:11 -0800

    Time: 334 ms (11.15%), Space: 56.1 MB (12.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,11 +3,42 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-   let result = {}
-   for (let str of strs) {
-       let key = str.split('').sort().join('')
-       if (key in result) result[key].push(str)
-       else result[key] = [str]
-   }
-   return Object.values(result)
+    const obj = {}; // create object to group anagrams in
+    // helper prime number finding function
+    const isPrime = num => {
+        if (num < 2) return false;
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+    // generate first 26 prime numbers in array
+    const alphaPrimes = () => {
+        const primes = [];
+        let i = 0;
+        // find 26 primes
+        while (primes.length < 26) {
+            if (isPrime(i)) primes.push(i);
+            i++;
+        }
+        return primes;
+    }
+
+    const hash = string => {
+        const primes = alphaPrimes()
+        const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        let product = 1;
+        for (let i = 0; i < string.length; i++) {
+            product *= primes[alphabet.indexOf(string[i])];
+        }
+        return product;
+    };
+    strs.forEach(word => {
+        const hashValue = hash(word);
+        if (!obj[hashValue]) {
+            obj[hashValue] = [];
+        }
+        obj[hashValue].push(word);
+    })
+    return Object.values(obj);
 };
