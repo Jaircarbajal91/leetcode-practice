58d3472605768dc269db8b9024af12de36cedeba
Jair Carbajal <jair.carbajal91@gmail.com>
2022-11-22 11:33:09 -0800

    Time: 110 ms (93.16%), Space: 55.9 MB (25.68%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let result = []
+    let product = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result.push(product)
+        product *= nums[i]
+    }
+    product = 1;
+    for (let j = nums.length - 1; j >= 0; j--) {
+        result[j] *= product;
+        product *= nums[j];
+    }
+    return result;
+};
