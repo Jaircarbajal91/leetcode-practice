a90cc474572b72710d3121dacd3670736cb2bfec
Jair Carbajal <jair.carbajal91@gmail.com>
2022-11-25 16:47:49 -0800

    Time: 78 ms (88.81%), Space: 46.3 MB (51.25%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    
+    function dfs (node, max, min) {
+        if (!node) return true
+        
+        if (node.val >= max || node.val <= min) return false
+        
+        return dfs(node.left, node.val, min) && dfs(node.right, max, node.val)
+    }
+    return dfs(root, Infinity, -Infinity)
+};
