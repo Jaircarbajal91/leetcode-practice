b4d1ca50bdec1080d641d69907430ff288ca1c83
Jair Carbajal <jair.carbajal91@gmail.com>
2022-11-28 13:14:48 -0800

    Time: 95 ms (92.65%), Space: 46.9 MB (73.44%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,19 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-  let longest = ''
-  let left = 0
-  let right = 0
-  while(left <= right && right < s.length) {
-      let currentString = ''
-      while(!currentString.includes(s[right]) && s[right]) {
-          currentString += s[right]
-          if (currentString.length > longest.length) longest = currentString
-          right++
-      }
-      left++
-      right = left
-  }
-  return longest.length
-};
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set();
+    let left = 0;
+    let maxSize = 0;
+
+    if (s.length === 0) return 0;
+    if (s.length === 1) return 1;
+
+    for (let i = 0; i < s.length; i++) {
+
+        while (set.has(s[i])) {
+            set.delete(s[left])
+            left++;
+        }
+        set.add(s[i]);
+        maxSize = Math.max(maxSize, i - left + 1)
+    }
+    return maxSize;
+}
