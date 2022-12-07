6397595c1fcc67ec6f29c844286a8249c0a65e14
Jair Carbajal <jair.carbajal91@gmail.com>
2022-12-07 08:04:27 -0800

    Time: 134 ms (38.28%), Space: 51.7 MB (5.15%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -4,13 +4,13 @@
  */
 var maxArea = function(height) {
     let [r, l] = [height.length - 1, 0]
-    let maxArea = 0
+    let maxArea = 0;
     while(l < r) {
-        const [left, right] = [height[l], height[r]]
-        const area = Math.min(left, right) * (r - l);
-        maxArea = Math.max(maxArea, area)
-        if (left < right) l++
-        else r--;
+        let [left, right] = [height[l], height[r]]
+        let area = Math.min(left, right) * (r - l)
+        maxArea = Math.max(area, maxArea)
+        if (left > right) r--
+        else l++
     }
     return maxArea;
 };
