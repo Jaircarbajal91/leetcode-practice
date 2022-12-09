c2230b22e08dd5ce02992b5f6160f12154ed4e03
Jair Carbajal <jair.carbajal91@gmail.com>
2022-12-09 11:50:34 -0800

    Time: 97 ms (91.28%), Space: 46.6 MB (79.02%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,18 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
-    let set = new Set()
-    let l = 0
+var lengthOfLongestSubstring = function(s) {
     let max = 0
+    let set = new Set()
+    let pointer = 0
     for (let i = 0; i < s.length; i++) {
-        const letter = s[i]
-        while(set.has(letter)) {
-            set.delete(s[l])
-            l++
+        let letter = s[i];
+        while (set.has(letter)) {
+            set.delete(s[pointer])
+            pointer++
         }
         set.add(letter)
         max = Math.max(max, set.size)
     }
     return max
-}
+};
