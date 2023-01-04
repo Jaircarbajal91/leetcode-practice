bdf42123e6b852ea533e7ed6c13e6a3826b8d5b5
Jair Carbajal <jair.carbajal91@gmail.com>
2023-01-04 15:17:03 -0800

    Time: 66 ms (17.78%), Space: 13.8 MB (61.81%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -12,5 +12,5 @@ class Solution:
                 val = stone1 - stone2
                 heapq.heappush(stones, val)
         if len(stones):
-            return stones.pop(0) * -1
+            return stones[0] * -1
         return 0
