9bbb062a87651da0181e51155807fc29aa2f9bea
Jair Carbajal <jair.carbajal91@gmail.com>
2023-01-05 14:36:59 -0800

    Time: 77 ms (76.36%), Space: 43.7 MB (95.04%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,18 +11,18 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let result = new ListNode()
-    let curr = result
-    while (list1 && list2) {
+    let dummy = new ListNode()
+    let result = dummy
+    while(list1 && list2) {
         if (list1.val < list2.val) {
-            curr.next = list1
+            dummy.next = list1
             list1 = list1.next
         } else {
-            curr.next = list2
+            dummy.next = list2
             list2 = list2.next
         }
-        curr = curr.next
+        dummy = dummy.next
     }
-    curr.next = list1 || list2
+    dummy.next = list1 || list2
     return result.next
 };
