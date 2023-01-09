df9ff0cb22a2f5568932749ecfa84c40df81cece
Jair Carbajal <jair.carbajal91@gmail.com>
2023-01-09 15:24:08 -0800

    Time: 87 ms (76.25%), Space: 47.1 MB (27.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -3,16 +3,23 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-   let storage = {}
-   for (let num of nums) {
-       if (num in storage) storage[num]++
-       else storage[num] = 1
-   }
-    let max = Object.entries(storage).sort((a,b) => b[1] - a[1])
-    max.length = k
-    return max.reduce((result, num) => {
-        result.push(Number(num[0]))
-        return result
-    }, [])
-};
+function topKFrequent(nums, k) {
+  // create a hash map to count the frequency of each element
+  const count = new Map();
+  for (const num of nums) {
+    count.set(num, (count.get(num) || 0) + 1);
+  }
+
+  // create an array of lists to store the elements with the same frequency
+  const buckets = Array.from({ length: nums.length + 1 }, () => []);
+  for (const [num, freq] of count) {
+    buckets[freq].push(num);
+  }
+
+  // concatenate the lists and return the top k elements
+  const result = [];
+  for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
+    result.push(...buckets[i]);
+  }
+  return result;
+}
