e3031fc255f1431784dd174cad128e5a6701d251
Jair Carbajal <jair.carbajal91@gmail.com>
2023-02-16 11:33:43 -0800

    Time: 67 ms (75.58%), Space: 42.7 MB (81.92%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -3,21 +3,15 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    let result = []
-    let subset = []
-    const dfs = (i) => {
-        if (i >= nums.length) {
-            result.push(subset.slice())
-            return
-        }
-        // decision to include nums[i]
-        subset.push(nums[i])
-        dfs(i + 1)
-        // decision to NOT include nums[i]
-        subset.pop()
-        dfs(i + 1)
-        
-    }
-    dfs(0)
-    return result
+  let result = [[]]
+  const dfs = (i, subset) => {
+      if (i >= nums.length) return
+      subset.push(nums[i])
+      dfs(i + 1, subset)
+      result.push(subset.slice())
+      subset.pop()
+      dfs(i + 1, subset)
+  }
+  dfs(0, [])
+  return result
 };
