5d7ddaac3cb332487570e1283e05aaa658143caf
Jair Carbajal <jair.carbajal91@gmail.com>
2023-02-17 09:41:13 -0800

    Time: 84 ms (39.35%), Space: 48.5 MB (10.81%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.js
+++ 0040-combination-sum-ii/0040-combination-sum-ii.js
@@ -5,20 +5,19 @@
  */
 var combinationSum2 = function(candidates, target) {
     let result = []
-    
     candidates.sort((a, b) => a - b)
-    const dfs = (i, path, pathSum) => {
+    const dfs = (idx, path, pathSum) => {
         if (pathSum > target) return
         if (pathSum === target) {
             result.push(path.slice())
             return
         }
-        for (let j = i; j < candidates.length; j++) {
-            if (j !== i && candidates[j] === candidates[j - 1]) continue
-            dfs(j + 1, [...path, candidates[j]], pathSum + candidates[j])
-        }
         
+        for (let i = idx; i < candidates.length; i++) {
+            if (i !== idx && candidates[i] === candidates[i - 1]) continue
+            dfs(i + 1, [...path, candidates[i]], pathSum + candidates[i])
+        }
     }
     dfs(0, [], 0)
-    return result;
+    return result
 };
