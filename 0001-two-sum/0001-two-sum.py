4417006ba0ceef664e228073e78fdb0c97d5e9da
Jair Carbajal <jair.carbajal91@gmail.com>
2023-09-11 11:34:51 -0700

    Time: 68 ms (64.51%), Space: 17.6 MB (50.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        storage = {}
+        set = {}
         for i, num in enumerate(nums):
-            if (target - num) in storage:
-                return [storage[target-num], i]
+            if target - num in set:
+                return [set.get(target - num), i] 
             else:
-                storage[num] = i
-        return -1
+                set.setdefault(num, i)
+        return []
